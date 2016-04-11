999d579d9bdff84b1598e98073907d18ea5ca033
Jenwich Rattanayenjai <pamjeavrat@gmail.com>
2016-04-11 16:27:52 +0700

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+
+var path = require('path');
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.listen(PORT, function() {
+    console.log("Listening PORT", PORT);
+});
+
+app.set('view engine', 'jade');
+app.set('views', path.resolve(__dirname, '../public/jade'));
+
+app.use(function(req, res, next) {
+    app.locals.pretty = true;
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
